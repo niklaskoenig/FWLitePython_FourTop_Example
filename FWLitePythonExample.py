7453946f82acec36f232c0383f278ae39fcbcac8
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2014-03-17 16:41:18 +0100

    added dataset name

--- FWLitePythonExample.py
+++ FWLitePythonExample.py
@@ -4,7 +4,7 @@ muonsLabel  = "muons"
 muonsHandle = Handle("vector<reco::Muon>")
 muonsPtHist = ROOT.TH1D("muonsPtHist","muonsPtHist",200,0,200)
 ####
-events = Events('dcap://grid-dcap.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/data/Run2012A/MultiJet/AOD/22Jan2013-v1/20000/0036C47E-0B74-E211-B992-00266CF32684.root')
+events = Events('dcap://grid-dcap.physik.rwth-aachen.de/pnfs/physik.rwth-aachen.de/cms/store/data/Run2012A/MultiJet/AOD/22Jan2013-v1/20000/0036C47E-0B74-E211-B992-00266CF32684.root') #part of /MultiJet/Run2012A-22Jan2013-v1/AOD
 ###
 for i,event in enumerate(events):
   event.getByLabel(muonsLabel,muonsHandle)
